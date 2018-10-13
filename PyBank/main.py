d9c9915c42c708852cf3d883e20eec54783453c4
Jacob Lingwall <lingwaja@nga.mil>
2018-10-12 20:31:47 -0500

    completed

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,6 +9,7 @@ csvdat = csv.reader(dat)
 csvdict = {}
 rowcount = 0
 total = 0.0
+change = 0
 avrage = 0
 low = 0
 hig = 0.0
@@ -18,7 +19,9 @@ for row in csvdat:
         continue
     r1 = int(row[1])
     total = total + r1
+    change = change + abs(r1)
     if r1 < low:
+        
         low = r1
     if r1 > hig:
         hig = r1
@@ -27,10 +30,11 @@ for row in csvdat:
 
     rowcount += 1
 lenc = len(csvdict)
+print(change)
 print("=========================Financial Analysis=============================")
 print("Total Months: " + str(lenc))
 print("Total: " + str(total))
-print("Average: " + str(total/lenc))
+print("Average Change +/-: " + str(change/lenc))
 print("Biggest Profit: " + str(hig))
 print("Biggest Loss: " + str(low))
 print("------------------------------------------------------------------------")
